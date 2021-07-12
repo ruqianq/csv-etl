fab6d1b59d232f70475384407d34dc603b5d4158
ruqianq <daisy@acts.net>
2021-07-12 13:36:39 -0400

    revise the output and test

--- models/output.py
+++ models/output.py
@@ -1,22 +1,11 @@
 from dataclasses import dataclass
-from typing import List
-
-
-@dataclass
-class FacilityLocation:
-    name: str
-    state: str
-    address: str
-    zipcode: str
-    country: str
-    purpose: str
 
 
 @dataclass
 class ToxicAirPollutionByCompany:
-    id: str
+    id: int
     report_date: str
     company_names: str
     toxic_air_pollution: int
-    facility_locations: List[FacilityLocation]
-    chemicals_released: List[str]
+    unit: str
+    description: str
