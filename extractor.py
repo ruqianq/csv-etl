189974c6c62a03cf7ee191c614edf9caf98f0f71
ruqianq <daisy@acts.net>
2021-07-13 16:47:20 -0400

    add rows

--- extractor.py
+++ extractor.py
@@ -7,13 +7,16 @@ from models.query_condition import QueryConditionByTable
 logger = logging.getLogger(__name__)
 
 
-def construct_query_str(query_conditions: [QueryConditionByTable], output_format: str = 'JSON') -> str:
+def construct_query_str(query_conditions: [QueryConditionByTable], output_format: str = 'JSON',
+                        rows: str = None) -> str:
     query_str = ''
     if query_conditions:
         for q in query_conditions:
             query_str += str(q)
     query_str = query_str
     logger.info(f'Query data based on {query_str}/{output_format}')
+    if rows:
+        return query_str + f'/{output_format}' + f'/rows/{rows}'
     return query_str + f'/{output_format}'
 
 
